d24a8b29ad9d6e32d01dcf463941ef4ba9c9de7a
Alex <alexgrubor@gmail.com>
2023-02-01 14:40:55 +0100

    added styling

--- recipe/src/pages/Drinks.jsx
+++ recipe/src/pages/Drinks.jsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import "../styles/drinks.css";
+import Top10NonAlc from "../components/Top10NonAlc";
 const Drinks = () => {
   const [drinks, setDrinks] = useState([]);
   const [isClicked, setIsClicked] = useState(false);
   const [query, setQuery] = useState("");
   const [ingQuery, setIngQuery] = useState("");
-  const [ing, setIng] = useState([])
   const urlSearchByName = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`;
-  const urlSearchByIng = `https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${ingQuery}`;
+  const urlSearchByIng = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingQuery}`;
 
   const onSearchNameHandler = (e) => {
     setQuery(e.target.value);
@@ -32,45 +32,49 @@ const Drinks = () => {
     axios
       .get(urlSearchByIng)
       .then((res) => {
-        console.log(res.data);
-        setIng(res.data.ingredients);
+        setDrinks(res.data.drinks);
       })
       .catch((err) => console.log(err));
   };
 
   return (
     <div>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-        }}
-        className="search-by-name"
-      >
-        <input
-          type="text"
-          placeholder="Search by name"
-          onChange={onSearchNameHandler}
-        />
-        <button type="submit" onClick={searchByNameHandler}>
-          Search
-        </button>
-      </form>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-        }}
-        className="search-by-ingredient"
-      >
-        <input
-          type="text"
-          placeholder="Search by ingredient"
-          onChange={onSearchIngHandler}
-        />
-        <button type="submit" onClick={onSearchHandlerByIng}>
-          {" "}
-          Search
-        </button>
-      </form>
+      <h1>Drinks</h1>
+      <div>
+        <h4>Cocktail Finder</h4>
+
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+          className="search-by-name"
+        >
+          <input
+            type="text"
+            placeholder="Search by name"
+            onChange={onSearchNameHandler}
+          />
+          <button type="submit" onClick={searchByNameHandler}>
+            Search
+          </button>
+        </form>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+          className="search-by-ingredient"
+        >
+          <input
+            type="text"
+            placeholder="Search by ingredient"
+            onChange={onSearchIngHandler}
+          />
+          <button type="submit" onClick={onSearchHandlerByIng}>
+            {" "}
+            Search
+          </button>
+        </form>
+      </div>
       <div className="results">
         {isClicked &&
           drinks !== null &&
@@ -85,6 +89,10 @@ const Drinks = () => {
               </div>
             );
           })}
+      </div>
+      <div className="top-10nonalc">
+        <h4>Top 10 Non-Alcoholic Drinks</h4>
+        <Top10NonAlc />
       </div>
     </div>
   );
--- recipe/src/pages/SingleDrink.jsx
+++ recipe/src/pages/SingleDrink.jsx
@@ -1,11 +1,14 @@
-import { useParams, useNavigate } from "react-router-dom"
-import axios from "axios"
-import { useEffect, useState } from "react"
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import {CiGlass } from 'react-icons/ci'
+import { FaGlassMartini} from 'react-icons/fa'
+import "../styles/singledrink.css";
 const SingleDrink = () => {
   const { id } = useParams();
   const navigate = useNavigate();
-  const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
-  const [drink, setDrink] = useState(null)
+  const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
+  const [drink, setDrink] = useState(null);
   useEffect(() => {
     axios
       .get(url)
@@ -15,35 +18,79 @@ const SingleDrink = () => {
           res.data.drinks === undefined ||
           res.data.drinks.length === 0
         ) {
-          navigate("/error")
+          navigate("/error");
         }
 
-        setDrink(res.data.drinks[0])
+        setDrink(res.data.drinks[0]);
       })
-      .catch((err) => console.log(err))
-  }, [])
-   
+      .catch((err) => console.log(err));
+  }, []);
 
-  
+  const ings = [
+    drink?.strIngredient1,
+    drink?.strIngredient2,
+    drink?.strIngredient3,
+    drink?.strIngredient4,
+    drink?.strIngredient5,
+    drink?.strIngredient6,
+    drink?.strIngredient7,
+    drink?.strIngredient8,
+  ];
+  const measure = [
+    drink?.strMeasure1,
+    drink?.strMeasure2,
+    drink?.strMeasure3,
+    drink?.strMeasure4,
+    drink?.strMeasure5,
+    drink?.strMeasure6,
+    drink?.strMeasure7,
+    drink?.strMeasure8,
+  ];
+
   return (
-    drink && <div>
-      <div className="single-drink-img">
-        <img src={drink.strDrinkThumb} alt={drink.strDrink} />
+    drink && (
+      <div className="single-drink-container">
+        <div className="name">
+          <h3>{drink.strDrink}</h3>
+          {
+            drink.strAlcoholic === "Alcoholic" ? (
+              <p className="alcoholic"> <FaGlassMartini/> Alcoholic</p>
+            ) : (
+              <p className="non-alcoholic"> <CiGlass/>  Non-Alcoholic</p>
+            )
+          }
+        </div>
+
+        <div className="single-drink-content">
+          <div className="single-drink-img">
+            <img src={drink.strDrinkThumb} alt={drink.strDrink} />
+          </div>
+          <div className="single-drink-details">
+            <ul>
+              {" "}
+              <p>Ingredients</p>
+              {ings.map((ing, index) => {
+                if (ing !== null) {
+                  return <li key={index}>{ing}</li>;
+                }
+              })}
+            </ul>
+            <ul>
+              <p>Measure</p>
+              {measure.map((m, index) => {
+                if (m !== null) {
+                  return <li key={index}>{m}</li>;
+                }
+              })}
+            </ul>
+          </div>
+        </div>
+        <div className="instr">
+          <h3>Instructions</h3>
+          <p>{drink.strInstructions}</p>
+        </div>
       </div>
-      <div className="single-drink-details">
-        <h3>{drink.strDrink}</h3>
-        <ul> Ingredients
-          <li>{drink.strIngredient1}</li>
-          <li>{drink.strIngredient2}</li>
-          <li>{drink.strIngredient3}</li>
-          <li>{drink.strIngredient4}</li>
-          <li>{drink.strIngredient5}</li>
-          <li>{drink.strIngredient6}</li>
-          <li>{drink.strIngredient7}</li>
-          <li>{drink.strIngredient8}</li>
-        </ul>
-      </div>
-    </div>
-  )
-}
-export default SingleDrink
+    )
+  );
+};
+export default SingleDrink;
